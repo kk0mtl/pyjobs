b7cae06a81d2a2ad2429484629a2ceba6ed6e012
kk0mtl <77203242+kk0mtl@users.noreply.github.com>
2023-03-08 14:48:30 +0000

    Initial commit

---
+++ extractors/indeed.py
@@ -0,0 +1,61 @@
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+options = Options()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+browser = webdriver.Chrome(options=options)
+
+
+def extract_pages_count(keyword):
+  base_url = "https://kr.indeed.com/jobs?q="
+  browser.get(f"{base_url}{keyword}")
+
+  soup = BeautifulSoup(browser.page_source, "html.parser")
+  pagination = soup.find("nav", role="navigation")
+  if pagination == None:  # site has only 1 page
+    return 1
+  pages = pagination.select("div a")
+  count = len(pages)
+
+  if count == 0:
+    return 1
+  elif count >= 5:
+    return 5
+  else:
+    return count
+
+
+def extract_indeed_jobs(keyword):
+  pages = extract_pages_count(keyword)
+  print("Found", pages, "pages")
+  results = []
+  for page in range(pages):
+    base_url = "https://kr.indeed.com/jobs"
+    final_url = f"{base_url}?q={keyword}&start={page*10}"
+    print("Requesting", final_url)
+    browser.get(f"{final_url}")
+
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    job_list = soup.find("ul", class_="jobsearch-ResultsList")
+    jobs = job_list.find_all('li', recursive=False)
+
+    for job in jobs:
+      zone = job.find("div", class_="mosaic-zone")
+      if zone == None:
+        anchor = job.select_one("h2 a")
+        title = anchor["aria-label"]
+        link = anchor["href"]
+        company = job.find("span", class_="companyName")
+        location = job.find("div", class_="companyLocation")
+
+        job_data = {
+          "link": f"https://kr.indeed.com{link}",
+          "company": company.string.replace(",", " "),
+          "location": location.string.replace(",", " "),
+          "position": title.replace(",", " ")
+        }
+        results.append(job_data)
+  return results
